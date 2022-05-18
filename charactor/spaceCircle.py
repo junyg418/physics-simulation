a0f859868b9000f704c0fe41c37d58eb2248644a
KIMYO9 <fgh33678@gmail.com>
2022-05-18 23:38:27 +0900

    최최종 무중력 시뮬

--- charactor/spaceCircle.py
+++ charactor/spaceCircle.py
@@ -13,20 +13,20 @@ class SpaceCircle:
     def x_posMove(self):
         self.p_x += self.v_x
         if self.v_x > 0: # 속도가 오른쪽 일 때
-            if self.p_x >=dis_x -12:
+            if self.p_x >=dis_x -18:
                 self.v_x = -self.v_x
         elif self.v_x < 0:
-            if self.p_x <= 12:
+            if self.p_x <= 18:
                 self.v_x = abs(self.v_x)
     def y_posMove(self):
         self.p_y += self.v_y
         if self.v_y > 0: # 속도가 오른쪽 일 때
-            if self.p_y >=dis_y -12:
+            if self.p_y >=dis_y -18:
                 self.v_y = -self.v_y
         elif self.v_y < 0:
-            if self.p_y <= 12:
+            if self.p_y <= 18:
                 self.v_y = abs(self.v_y)
 
     def draw(self):
         self.display.fill((255,255,255))
-        pygame.draw.circle(self.display, (0,0,255), (self.p_x, self.p_y), 20)
+        pygame.draw.circle(self.display, (0,0,0), (self.p_x, self.p_y), 20)

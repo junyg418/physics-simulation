ce5ae2dc1ed03af6567b2c144903fd59d119eb6f
KIMYO9 <fgh33678@gmail.com>
2022-05-18 22:37:43 +0900

    간단한 버그 수정

--- charactor/spaceCircle.py
+++ charactor/spaceCircle.py
@@ -13,18 +13,18 @@ class SpaceCircle:
     def x_posMove(self):
         self.p_x += self.v_x
         if self.v_x > 0: # 속도가 오른쪽 일 때
-            if self.p_x >=dis_x -7:
+            if self.p_x >=dis_x -12:
                 self.v_x = -self.v_x
         elif self.v_x < 0:
-            if self.p_x <= 7:
+            if self.p_x <= 12:
                 self.v_x = abs(self.v_x)
     def y_posMove(self):
         self.p_y += self.v_y
         if self.v_y > 0: # 속도가 오른쪽 일 때
-            if self.p_y >=dis_y -7:
+            if self.p_y >=dis_y -12:
                 self.v_y = -self.v_y
         elif self.v_y < 0:
-            if self.p_y <= 7:
+            if self.p_y <= 12:
                 self.v_y = abs(self.v_y)
 
     def draw(self):

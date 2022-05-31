1b9e045d28788275b78a39af573e20c891b5e5f1
KIMYO9 <fgh33678@gmail.com>
2022-05-31 20:31:40 +0900

    2022-05-31

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ tan_bool = False
 space_bool = False
 while True:
     moveType = input('''
-    완전탄성충돌 : 1
-    포물선 통통 : 2
-    무중력 시뮬 : 3
-    종료 : 아무 키나 입력
-    >>>''')
+완전탄성충돌 : 1
+포물선 통통 : 2
+무중력 시뮬 : 3
+종료 : 아무 키나 입력
+>>>''')
 
     if moveType == '1':
         move = 0 # 'wantanchung'
@@ -39,13 +39,13 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-# display_x = 640
-# display_y = 440
-display_x = 1400
-display_y = 600
+
+size = (640, 440)
+display_x, display_y = size 
 GameDisplay = pygame.display.set_mode((display_x, display_y))
 GameDisplay.fill(WHITE)
 pygame.display.set_caption("Gravity Falls")
+
 
 if tan_bool:
     circle.dis_x = display_x
@@ -77,6 +77,11 @@ if space_bool:
 
 while space_bool:
     pygame.display.update()
+    # if abs(spacePlayer.v_x)
+
+    font = pygame.font.SysFont("arial", 20)
+    text = font.render(f'x축 속도: {abs(spacePlayer.v_x)} y축 속도: {abs(spacePlayer.v_y)}', True, BLACK)
+    GameDisplay.blit(text, (0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -88,18 +93,18 @@ while space_bool:
             elif event.key == K_LEFT:
                 # spacePlayer.v_x -= 0.1
                 spacePlayer.v_x = round(spacePlayer.v_x - 0.1, 1)
-                print(spacePlayer.v_x)
+                print(f'x{spacePlayer.v_x}')
             elif event.key == K_RIGHT:
                 # spacePlayer.v_x += 0.1
                 spacePlayer.v_x = round(spacePlayer.v_x + 0.1, 1)
-                print(spacePlayer.v_x)
+                print(f'x{spacePlayer.v_x}')
             elif event.key == K_UP:
                 # spacePlayer.v_y += 0.1
                 spacePlayer.v_y = round(spacePlayer.v_y - 0.1, 1)
-                print(spacePlayer.v_y)
+                print(f'y{spacePlayer.v_y}')
             elif event.key == K_DOWN:
                 spacePlayer.v_y = round(spacePlayer.v_y + 0.1, 1)
-                print(spacePlayer.v_y)
+                print(f'y{spacePlayer.v_y}')
         
     FramePerSec.tick(FPS)
     spacePlayer.x_posMove()
